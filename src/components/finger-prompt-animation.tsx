3ead1ba7f2d4239bdfb494ecc8939daf2c91b34d
Loli Annabeth <skywalker338520@gmail.com>
2026-01-17 16:29:56 +0000

    No the shape of the icon does not match the image I sent out. Please dir

--- src/components/finger-prompt-animation.tsx
+++ src/components/finger-prompt-animation.tsx
@@ -10,10 +10,12 @@ const FingerPromptAnimation = () => {
                 fill="none"
                 stroke="white"
                 strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 xmlns="http://www.w3.org/2000/svg"
-                className="transform rotate-180"
             >
-                <path d="M13.5 16V5.32789C13.5 4.59549 12.9045 4 12.1721 4C11.4397 4 10.8442 4.59549 10.8442 5.32789V14.5H9C7.89543 14.5 7 15.3954 7 16.5V20C7 21.1046 7.89543 22 9 22H15C16.1046 22 17 21.1046 17 20V17.5C17 16.6716 16.3284 16 15.5 16H13.5Z" />
+                <path d="M9 7.04211C9 5.91789 9.89543 5 11 5H13C14.1046 5 15 5.91789 15 7.04211V13.7601C15 14.8843 14.1046 15.8022 13 15.8022H11C9.89543 15.8022 9 14.8843 9 13.7601V7.04211Z" />
+                <path d="M12 15.8022V19" />
             </svg>
         </div>
     );
