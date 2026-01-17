4b79075884b00370975ef5ca4acb9c4659cf6156
Loli Annabeth <skywalker338520@gmail.com>
2026-01-17 16:26:54 +0000

    I want the icon exactly look like this, in white line

--- src/components/finger-prompt-animation.tsx
+++ src/components/finger-prompt-animation.tsx
@@ -7,7 +7,9 @@ const FingerPromptAnimation = () => {
                 width="64"
                 height="64"
                 viewBox="0 0 24 24"
-                fill="white"
+                fill="none"
+                stroke="white"
+                strokeWidth="1.5"
                 xmlns="http://www.w3.org/2000/svg"
                 className="transform rotate-180"
             >
